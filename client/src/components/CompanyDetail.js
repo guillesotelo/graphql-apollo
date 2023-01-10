ab491ba13514508e4e93b4bc26ad5f85ee070f6c
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-01-10 15:14:55 +0100

    first push

---
+++ client/src/components/CompanyDetail.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { getCompany } from '../graphql/queries';
+
+function CompanyDetail() {
+  const [company, setCompany] = useState(null)
+  const { companyId } = useParams();
+  
+  useEffect(() => {
+    getCompany(companyId).then(setCompany)
+  }, [companyId])
+
+  if (!company) return <p>Loading...</p>
+
+  return (
+    <div>
+      <h1 className="title">
+        {company.name}
+      </h1>
+      <div className="box">
+        {company.description}
+      </div>
+    </div>
+  );
+}
+
+export default CompanyDetail;
---
+++ server/resolvers.js
@@ -0,0 +1,12 @@
+import { Company, Job } from "./db.js"
+
+export const resolvers = {
+    Query: {
+        jobs: () => Job.findAll(),
+        job: (_root, { id }) => Job.findById(id),
+        company: (_root, { id }) => Company.findById(id)
+    },
+    Job: {
+        company: (job) => Company.findById(job.companyId)
+    }
+}
